c2fc2e7123a66a14ee88a7531f0db9c705903187
Théo Bernard <theo.bernard.94100@gmail.com>
2020-04-02 17:49:05 +0200

    Setup Server

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const https = require("https");
+
+const app = express();
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.listen(3000, function() {
+  console.log("Weather app listening on port 3000!");
+});
