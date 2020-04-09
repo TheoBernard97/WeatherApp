ef236e099f10c8d6463cc715002a836f1fc71bc8
Théo Bernard <theo.bernard.94100@gmail.com>
2020-04-09 11:23:38 +0200

    Feater : Show the current date

--- app.js
+++ app.js
@@ -3,13 +3,45 @@ const https = require("https");
 const bodyParser = require("body-parser");
 
 const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", function(req, res) {
-  res.sendFile(__dirname + "/index.html");
+app.get("/", function (req, res) {
+  const days = [
+    "Dimanche",
+    "Lundi",
+    "Mardi",
+    "Mercredi",
+    "Jeudi",
+    "Vendredi",
+    "Samedi",
+  ];
+  const months = [
+    "Janvier",
+    "Fevrier",
+    "Mars",
+    "Avril",
+    "Mai",
+    "Juin",
+    "Juillet",
+    "Aout",
+    "Septembre",
+    "Octobre",
+    "Novembre",
+    "Decembre",
+  ];
+  const date = new Date();
+  const weekDay = days[date.getDay()];
+  const numberDay = date.getDate();
+  const month = months[date.getMonth()];
+  const currentDay = weekDay + " " + numberDay + " " + month;
+  res.render("index", { currentDay: currentDay });
 });
 
-app.post("/", function(req, res) {
+app.post("/", function (req, res) {
   const apiKey = "1f6f5986763c0de8cf5133ef6670b28a";
   const query = req.body.cityName;
   const unit = "metric";
@@ -21,11 +53,11 @@ app.post("/", function(req, res) {
     "&appid=" +
     apiKey;
 
-  https.get(url, function(response) {
+  https.get(url, function (response) {
     if (response.statusCode === 200) {
       console.log("No problem.");
 
-      response.on("data", function(data) {
+      response.on("data", function (data) {
         const weatherData = JSON.parse(data);
         const temp = weatherData.main.temp;
         const iconTag = weatherData.weather[0].icon;
@@ -49,6 +81,6 @@ app.post("/", function(req, res) {
   });
 });
 
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log("Weather app listening on port 3000!");
 });
